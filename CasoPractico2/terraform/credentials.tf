05dbe1e843ed58328b793a43a73025b6527f2b56
ja-rejas <76849703+ja-rejas@users.noreply.github.com>
2021-03-02 13:24:08 +0100

    Delete credentials.tf

--- CasoPractico2/terraform/credentials.tf
+++
@@ -1,9 +0,0 @@
-# Crea un service principal y rellena los siguientes datos para autenticar
-provider "azurerm" {
- features {}
- subscription_id = "63d8cdc3-9f78-4424-983d-8547a6c03b5b"
- client_id       = "http://JARG_SP"
- client_secret   = "f.XQlJFFxGN4m-1hRVIqSvRkGh1DBwb4GP"
- tenant_id       = "899789dc-202f-44b4-8472-a6d40f9eb440"
-}
-
