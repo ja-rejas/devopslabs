f7aebe3ce526f56cdf259eea71259fb6d151e647
Jose Alejandro Rejas <josealejandro.rejas960@comunidadunir.net>
2021-03-04 04:30:19 +0100

    Subimos al repositorio los avances del apartado de anible (configuración para despliegue de un cluster de kubernetes)

--- CasoPractico2/terraform/vars.tf
+++ CasoPractico2/terraform/vars.tf
@@ -4,12 +4,18 @@ variable "location" {
 	default = "West Europe"
 }
 
+# Si posteriormente necesitamos modificar el tipo de máquina a instalar, será en est pundo donde lo tenemos
+# que hacer. De este modo, solo tenemos que cambiarlo en este punto. Esto también implicará que todas las VMs 
+# que provisionemos tendrán las mismas características.
 variable "vm_size" {
 	type = string
 	description = "Tamaño de las máquinas virtuales"
 	default = "Standard_D1_v2" #3.5 GB, 1 CPU
 }
 
+# En la variable vms, definiremos el array con los nombres de las máquinas virtuales que queremos instalar
+# se utilizarán tanto los nombres como los índices de esta varieble para configurar los elementos repetitivos
+# como por ejemplos las NICs, las IpPublicas, las propias máquinas virtuales, etc.
 variable "vms" {
 	description = "Máquinas virtuales a crear"
 	type = list(string)
