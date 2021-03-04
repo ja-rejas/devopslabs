db4e9fc51ebe95d5105301766ccc45cb3347038e
ja-rejas <76849703+ja-rejas@users.noreply.github.com>
2021-03-04 04:33:15 +0100

    Delete credentials2.tf

--- CasoPractico2/terraform/credentials2.tf
+++
@@ -1,9 +0,0 @@
-# Crea un service principal y rellena los siguientes datos para autenticar
-provider "azure" {
- features {}
- subscription_id = "621c77dc-42a7-4144-8a44-c0638fec3c14"
- client_id       = "63d8cdc3-9f78-4424-983d-8547a6c03b5b"
- client_secret   = "MUj2MrdJ~IYiK2jZ0DeoY3mYiXiMFmA5JT"
- tenant_id       = "899789dc-202f-44b4-8472-a6d40f9eb440"
-}
-
